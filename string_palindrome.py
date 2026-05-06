2d0458e43b6838867a5e0964e60ca5b4de7af644
Dhatri <156882180+Dhatri0612@users.noreply.github.com>
2026-05-07 00:53:38 +0530

    Add files via upload

---
+++ string_palindrome.py
@@ -0,0 +1,9 @@
+text = input("Enter a string: ")
+reverse = ""
+for ch in text:
+    reverse = ch + reverse
+
+if text == reverse:
+    print("Palindrome String")
+else:
+    print("Not a Palindrome String")
